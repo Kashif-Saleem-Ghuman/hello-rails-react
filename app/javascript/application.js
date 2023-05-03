681fa06cb1b1fa6a8ae6fb582318dcc5d2d1d1bf
unknown <kashif_saleem_23@hotmail.com>
2023-05-03 09:58:49 +0500

    Update application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,11 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import * as bootstrap from "bootstrap"
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (<h1>Hello World!</h1>);
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root'),
+);
